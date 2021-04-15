4b2897ff7dbb5dfdbc3f2f6c830e8cb217dc584f
37494 <vahagchelsea@gmail.com>
2021-04-15 23:38:17 +0400

    mi ban arten greci bayc nor chyuxum
    # Conflicts:
    #	src/com/company/Main.java
    
    TAZA_KLAZZ -um stexceciqn popoxakanner , contruktor, get+set

--- src/com/company/TAZA_Klazz.java
+++ src/com/company/TAZA_Klazz.java
@@ -2,9 +2,27 @@ package com.company;
 
 public class TAZA_Klazz {
 
+    String x;
+    String y;
 
+    public TAZA_Klazz(String x, String y) {
+        this.x = x;
+        this.y = y;
+    }
 
+    public String getX() {
+        return x;
+    }
 
+    public void setX(String x) {
+        this.x = x;
+    }
 
+    public String getY() {
+        return y;
+    }
 
+    public void setY(String y) {
+        this.y = y;
+    }
 }
